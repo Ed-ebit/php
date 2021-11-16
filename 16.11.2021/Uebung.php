8b1b3e413683ff433698f475d50cebb3bfe91db0
Your Name <you@example.com>
2021-11-16 18:56:00 +0100

    sadf

--- 16.11.2021/Uebung.php
+++ 16.11.2021/Uebung.php
@@ -18,7 +18,19 @@ $string2="folgende Kalenderwoche: ";
 $string3="";
 $string4="Bananen, 5 Kilo für nur 5,- Euro!";
 
-printf($string1."%'-3s",$string2."%'-3s",$string3."%'*5s",$string3);
+printf($string1.'---'.$string2.'---'."%'*42s",$string3.$string3.'---'.$string4);
+
+$string=$string1.$string2.$string3.$string4;
+echo var_dump($string).'<br>';
+$stringex = explode(" ",$string);
+echo var_dump($stringex).'<br>';
+$stringim = implode('#', $stringex);
+echo var_dump($stringim).'<br>';
+
+$string5 = str_replace ('das Angebot','<br>unser Sonderangebot</b>',$string1);
+$string6 = str_replace ('Bananen','<i>Alle Obstsorten</i>',$string4);
+
+printf($string5.'---'.$string2.'---'."%'*57s",$string3.$string3.'---'.$string6);
 
 ?>
     
